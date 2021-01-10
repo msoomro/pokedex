2881292d9d826d5bc784bf3214935c4fbc00dd5a
Moomal <moomalmsoomro@gmail.com>
2021-01-10 17:45:45 -0500

    Created API.get_data method. Gets data from Pokeapi and parses into array of hashes with pokemon info.

---
+++ lib/pokedex/api.rb
@@ -0,0 +1,11 @@
+class API
+
+    def self.get_data
+        response = RestClient.get("https://pokeapi.co/api/v2/pokemon/")
+        pokemon_array = JSON.parse(response)["results"]
+        pokemon_array.each do |pokemon|
+            Pokemon.new(pokemon)
+        end
+    end
+
+end
