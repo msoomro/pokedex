bbf4cdeabc36a6df09d2c1db9ca5bc5d89473df2
Moomal <moomalmsoomro@gmail.com>
2021-05-19 04:18:55 -0400

    Modify menu options so that they are more descriptive and clear.

--- lib/pokedex/cli.rb
+++ lib/pokedex/cli.rb
@@ -1,4 +1,5 @@
 class CLI
+    MAXID = 1083
 
     def start
         greeting
@@ -29,13 +30,13 @@ class CLI
     end
 
     def user_input
-        gets.strip
+        gets.strip.downcase
     end
 
     def menu
         puts "\n\n\t☆  M  E  N  U  ☆\n\n"
-        #puts "What would you like to see?\n"
-        puts "Type \'list\' to list all Pokemon."
+
+        puts "Type \'list\' to list Pokemon names."
         puts "Type a Pokemon name to learn more about that Pokemon."
         puts "Type \'exit\' to close the Pokedex."
         puts "\n"
@@ -65,24 +66,52 @@ class CLI
     def list_pokemon(i_start, max)
         i_max = max
         Pokemon.all.each.with_index(1) do |pokemon, i|
-            if i >= i_start && i < i_max
-                puts "#{i}. #{pokemon.name}"
-            elsif i == i_max
+            #binding.pry
+            if i == MAXID
                 puts "#{i}. #{pokemon.name}\n"
-                puts "\n<--|'menu'\t <type to navigate>\t'next'|-->\n"
+                puts "\tEND OF LIST\n\n"
+                
+                puts "Type a Pokemon's name to see details about that Pokemon.\n"
+                puts "Type \'menu\' to return to the main menu.\n\n"
 
                 loop do
                     selection = user_input
                     
-                    if selection == 'menu'
-                        break
-                    elsif selection == 'next'
-                        i_max = i_max + max
+                    if selection == "menu"
                         break
                     else
-                        puts "Type 'next' or 'menu'"
+                        if !pokemon_selection(selection)
+                            puts "Type a Pokemon's name to see details about that Pokemon.\n"
+                            puts "Type \'menu\' to return to the main menu.\n\n"
+                        end
                     end
                 end
+            elsif i >= i_start && i < i_max
+                puts "#{i}. #{pokemon.name}"
+            elsif i == i_max
+                puts "#{i}. #{pokemon.name}\n"
+                puts "\nType 'more' to list #{max} more Pokemon."
+                puts "Type a Pokemon's name to see details about that Pokemon.\n"
+                puts "Type \'menu\' to return to the main menu.\n\n"
+
+                loop do
+                    selection = user_input
+                    
+                    if selection == "more"
+                        i_max = i_max + max
+                        break
+                    elsif selection == "menu"
+                        break
+                    else
+                        if !pokemon_selection(selection)
+                            puts "\nType 'more' to list #{max} more Pokemon."
+                            puts "Type a Pokemon's name to see details about that Pokemon.\n"
+                            puts "Type \'menu\' to return to the main menu.\n\n"
+                        end
+                    end
+                end
+            else
+                break
             end
         end
     end
@@ -93,8 +122,8 @@ class CLI
             pokemon.get_data
             pokemon_detail(pokemon) 
         else
-            puts "\nHmm, #{selection} is not a valid Pokemon. Try again?"
-            menu
+            puts "\nHmm, #{selection} is not a valid command or Pokemon name. Try again?"
+            nil
         end
     end
 
@@ -105,7 +134,8 @@ class CLI
         puts "Base XP: #{pokemon.base_experience}"
         puts "Abilities: #{pokemon.abilities.collect {|ability| ability["ability"]["name"]}.join(', ')}"
         puts "\n─────────────── *.·:·.☆*☆*☆*☆.·:·.*───────────────\n\n"
-        puts "\n<--| 'back'\t  <type to navigate>  \t'locations'|-->\n"
+        puts "\nType 'locations' to list locations where #{pokemon.name.capitalize} are found."
+        puts "Type 'back' to return to listing Pokemon.\n\n"
         pokemon_location(pokemon)
     end
 
